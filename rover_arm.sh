04801ab2ec412b23409aa473f2206e1948136132
Rover_NUC <iitbmartian@gmail.com>
2022-10-28 23:56:49 +0530

    Silly Mistake 3

--- rover_arm.sh
+++ rover_arm.sh
@@ -1,6 +1,6 @@
 #!/bin/sh 
 cd ~/ws/rover_ws;
-source devel/setup.bash;
+source devel/setup.zsh;
 # echo 'Please enter ROS_MASTER_URI';
 # read ROS_MASTER_URI_READ;
 # export ROS_MASTER_URI=$ROS_MASTER_URI_READ;
