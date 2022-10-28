395357e84c984f06c7218225febc4b1c22b827c2
VR-1103 <96605598+VR-1103@users.noreply.github.com>
2022-10-28 23:50:57 +0530

    Add files via upload

---
+++ rover_measure.sh
@@ -0,0 +1,9 @@
+#!/bin/sh 
+cd ~/ws/rover_ws;
+source devel/setup.bash;
+# echo 'Please enter ROS_MASTER_URI';
+# read ROS_MASTER_URI_READ;
+# export ROS_MASTER_URI=$ROS_MASTER_URI_READ;
+# export ROS_MASTER_URI=http://labpc:11311/
+echo 'Voltage Measurement'
+roslaunch rover_measure measure.launch
